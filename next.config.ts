26e20fd2cd857bff4260710231d49fa175cc5420
Fabio <cozzafa@gmail.com>
2026-02-17 22:34:35 +0100

    ignore typescript errors in build

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,9 @@
-import type { NextConfig } from "next"; 
-const nextConfig: NextConfig = { eslint: { ignoreDuringBuilds: true } }; 
-export default nextConfig; 
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default nextConfig;
